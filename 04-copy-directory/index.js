6e215f5251230f270b3572a91860191e4377814a
TolmaSh <m0v4anua@gmail.com>
2021-11-07 17:36:31 +0200

    feat: add fourth test

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
 
+const path = require("path");
+const fsPromises = require("fs/promises");
+
+async function createCopy() {
+    const from = path.join(__dirname, "files");
+    const src = path.join(__dirname, "files-copy");
+    await fsPromises.mkdir(src, {
+        recursive: true,
+    });
+    const current_files = await fsPromises.readdir(src, "utf-8");
+    for (let file of current_files) {
+        await fsPromises.rm(path.join(src, file));
+    }
+
+    const files = await fsPromises.readdir(from, "utf-8");
+    for (let file of files) {
+        await fsPromises.copyFile(path.join(from, file), path.join(src, file));
+    }
+}
+createCopy();
